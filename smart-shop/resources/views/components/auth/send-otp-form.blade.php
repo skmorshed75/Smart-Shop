10b0bab0eed4da2ab3410dac466042dccd900d86
Morshed <skmorshed75@yahoo.com>
2023-08-23 22:16:58 +0600

    Sending OTP verification 16 done

--- smart-shop/resources/views/components/auth/send-otp-form.blade.php
+++ smart-shop/resources/views/components/auth/send-otp-form.blade.php
@@ -3,18 +3,41 @@
         <div class="col-md-7 col-lg-6 center-screen">
             <div class="card animated fadeIn w-90  p-4">
                 <div class="card-body">
-                    <h4>EMAIL ADDRESS</h4>
-                    <br/>
-                    <label>Your email address</label>
-                    <input id="email" placeholder="User Email" class="form-control" type="email"/>
-                    <br/>
-                    <button onclick="VerifyEmail()"  class="btn w-100 float-end btn-primary">Next</button>
+                    <form action="" onsubmit="return false">
+                        <h4>EMAIL ADDRESS</h4>
+                        <br/>
+                        <label>Your email address</label>
+                        <input id="email" autofocus placeholder="User Email" class="form-control" type="email"/>
+                        <br/>
+                        <button onclick="VerifyEmail()"  class="btn w-100 float-end btn-primary">Next</button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
 <script>
+    async function VerifyEmail(){
+        let email = document.getElementById('email').value;
+
+        if(email.length == 0 ){
+            errorToast("Email is Required");
+        } else {
+            showLoader();
+            let result = await axios.post("/send-otp",{email:email});
+            hideLoader();
+            if(result.status === 200){
+                successToast(result.data['message']); 
+                sessionStorage.setItem('email',email);
+                setTimeout(function(){
+                    window.location.href = "/verifyOtp";
+                },1000);
+            } else {
+                errorToast("Email not Found");
+            }
+        }
+    }
+    /*
     async function VerifyEmail() {
         let email = document.getElementById('email').value;
         if(email.length === 0) {
@@ -33,4 +56,5 @@
             }
         }
     }
+    */
 </script>
--- smart-shop/resources/views/components/auth/verify-otp-form.blade.php
+++ smart-shop/resources/views/components/auth/verify-otp-form.blade.php
@@ -3,18 +3,25 @@
         <div class="col-md-7 col-lg-6 center-screen">
             <div class="card animated fadeIn w-90  p-4">
                 <div class="card-body">
-                    <h4>ENTER OTP CODE</h4>
-                    <br/>
-                    <label>4 Digit Code Here</label>
-                    <input id="code" placeholder="Code" class="form-control" type="text"/>
-                    <br/>
-                    <button onclick="VerifyOtp()"  class="btn w-100 float-end btn-primary">Next</button>
+                    <form action="">
+                        <h4>ENTER OTP CODE</h4>
+                        <br/>
+                        <label>4 Digit Code Here</label>
+                        <input id="code" autofocus placeholder="Code" class="form-control" type="text"/>
+                        <br/>
+                        <button onclick="VerifyOtp()"  class="btn w-100 float-end btn-primary">Next</button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
 <script>
+    async function VerifyOtp(){
+        
+    }
+
+    /*
     async function VerifyOtp() {
         let code = document.getElementById('code').value;
         if(code.length!==4) {
@@ -35,4 +42,5 @@
             }
         }
     }
+    */
 </script>
