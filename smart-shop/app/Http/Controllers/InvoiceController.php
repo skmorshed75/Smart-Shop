42ca0d7607aff9e7108dcb406518e7b3bdfc30ec
Morshed <skmorshed75@yahoo.com>
2023-09-11 23:52:24 +0600

    Invoice Back-end (invoice create / delete / list / details) testing with postman 41

--- smart-shop/app/Http/Controllers/InvoiceController.php
+++ smart-shop/app/Http/Controllers/InvoiceController.php
@@ -88,6 +88,7 @@ class InvoiceController extends Controller
         DB::beginTransaction();
         try{
             $user_id = $request->header('id');
+            //echo $user_id . "  " . $request->input('inv_id');
             InvoiceProduct::where('invoice_id',$request->input('inv_id'))
                 ->where('user_id',$user_id)
                 ->delete();
--- smart-shop/app/Models/invoice.php
+++ smart-shop/app/Models/invoice.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Customer;
+use App\Models\InvoiceProduct;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,10 +13,11 @@ class invoice extends Model
     use HasFactory;
     protected $fillable = ['total', 'discount', 'vat', 'payable', 'user_id', 'customer_id'];
 
-    function customer():BelongsTo{
+    function customer():belongsTo{
         return $this->belongsTo(Customer::class);
-    };
+    }
 
+    // function invoice_product():BelongsTo{
     // function invoice_product():BelongsTo{
     //     return $this->hasMany(InvoiceProduct::class);
     // }
